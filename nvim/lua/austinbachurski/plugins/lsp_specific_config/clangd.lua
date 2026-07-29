0a0ad0fe1f93e847edb9c7218faaae0648a9cb9a
Austin Bachurski <austinbachurski@gmail.com>
2026-07-29 13:57:55 -0600

    Update clangd target and add modules flag.

--- nvim/lua/austinbachurski/plugins/lsp_specific_config/clangd.lua
+++ nvim/lua/austinbachurski/plugins/lsp_specific_config/clangd.lua
@@ -1,11 +1,12 @@
 local format_group = vim.api.nvim_create_augroup("ClangFormatOnSave", { clear = true })
 
 vim.lsp.config("clangd", {
-   cmd = {
-      "/usr/sbin/clangd",
-      "--clang-tidy",
-      "--header-insertion=iwyu",
-   },
+  cmd = {
+    "/usr/bin/clangd",
+    "--clang-tidy",
+    "--header-insertion=iwyu",
+    "--experimental-modules-support",
+  },
 })
 
 vim.api.nvim_create_autocmd("BufWritePre", {
